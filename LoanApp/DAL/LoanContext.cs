2658dca28b1abe037e469a1195df203ae1e165d5
Samvel Belyan <Samvel_Belyan@epam.com>
2022-06-22 22:27:03 +0400

    add context

---
+++ LoanApp/DAL/LoanContext.cs
@@ -0,0 +1,21 @@
+﻿using LoanApp.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace LoanApp.DAL;
+
+public class LoanContext : DbContext
+{
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Credit> Credits { get; set; }
+
+    public LoanContext()
+    {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=EPAMYERW011B;Database=LoanDB;Trusted_Connection=True;");
+    }
+}
